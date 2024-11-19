efa9af06662cea01f081d39559c4fa601603afdf
Dhruv Bansal <dhruvbansalup@gmail.com>
2024-11-19 18:39:23 +0530

    Add PUT and Delete api to F006 App

--- Flask_Basics/F006_Flask-RESTful-API_Articles_App/application/api.py
+++ Flask_Basics/F006_Flask-RESTful-API_Articles_App/application/api.py
@@ -26,6 +26,8 @@ create_user_parser=reqparse.RequestParser()
 create_user_parser.add_argument('username')
 create_user_parser.add_argument('email')
 
+update_user_parser=reqparse.RequestParser()
+update_user_parser.add_argument('email')
 
 
 class userAPI(Resource):
@@ -33,16 +35,19 @@ class userAPI(Resource):
     def get(self, username):
         user=db.session.query(User).filter(User.username==username).first()
         if user:
+            #return valid json
             return user,200
         else:
             raise NotFoundError(status_code=404)
 
     @marshal_with(user_output_fields)
     def post(self):
+        #Use parser to get data
         args=create_user_parser.parse_args()
         username=args.get("username",None)
         email=args.get("email",None)
-
+        
+        # Validate imput data
         if username is None:
             raise BusinessValidationError(status_code=400,error_code="BE1001",error_message="username is required")
         if email is None:
@@ -50,21 +55,65 @@ class userAPI(Resource):
         elif "@" not in email:
             raise BusinessValidationError(status_code=400,error_code="BE1003",error_message="email is invalid")
 
+        #Check if user already exists
         user=db.session.query(User).filter((User.username==username)|(User.email==email)).first()
 
         if user:
             raise BusinessValidationError(status_code=400,error_code="BE1004",error_message="Duplicate User")
         
+        #add new user to database
         new_user=User(username=username,email=email)
         db.session.add(new_user)
         db.session.commit()
+
         return new_user,201
-
-    def put(self, username):
-        pass
+    
     def delete(self, username):
-        pass
+        #Check if user exists
+        user=db.session.query(User).filter(User.username==username).first()
+        
+        if user is None:
+            raise NotFoundError(status_code=404)
+        
+        #Check if there are articles for this user, if yes then throw error
+        article=Article.query.filter(Article.authors.any(username=username)).first()
+        
+        if article:
+            raise BusinessValidationError(status_code=400,error_code="BE1005",error_message="Can't Delete User as there are articles written by this user")
+        
+        #if no Dependency, then delete
+        db.session.delete(user)
+        db.session.commit()
 
+        return "Successfully Deleted",200
+
+    @marshal_with(user_output_fields)
+    def put(self, username):
+        # Use parser to get data
+        args = update_user_parser.parse_args()
+        new_email = args.get("email", None)
+
+        # Validate input data
+        if new_email is None:
+            raise BusinessValidationError(status_code=400, error_code="BE1002", error_message="email is required")
+        elif "@" not in new_email:
+            raise BusinessValidationError(status_code=400, error_code="BE1003", error_message="email is invalid")
+
+        # Check if same email already exists
+        existing_email = db.session.query(User).filter(User.email == new_email).first()
+        if existing_email:
+            raise BusinessValidationError(status_code=400, error_code="BE1006", error_message="Duplicate Email ID")
+
+        # Check if user exists
+        user = db.session.query(User).filter(User.username == username).first()
+        if user is None:
+            raise NotFoundError(status_code=404)
+
+        # Update user details
+        user.email = new_email
+        db.session.commit()
+
+        return user, 200
 
 
 class AllTableViewAPI(Resource):
